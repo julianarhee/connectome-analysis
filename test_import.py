7e4f46214cba42470dd53f6a902269c59f8d83a0
Juliana Rhee <juliana.rhee@gmail.com>
2025-09-22 16:50:15 -0400

    update project info

--- test_import.py
+++ test_import.py
@@ -3,12 +3,12 @@ import sys
 import os
 
 # Test the path calculation
-libs_path = os.path.join(os.getcwd(), 'libs', 'src')
+libs_path = os.path.join(os.getcwd(), 'libs')
 print('Current directory:', os.getcwd())
 print('Libs path:', libs_path)
 print('Path exists:', os.path.exists(libs_path))
 if os.path.exists(libs_path):
-    print('Files in libs/src:', os.listdir(libs_path))
+    print('Files in libs/:', os.listdir(libs_path))
 
 # Add to path and test import
 sys.path.insert(0, libs_path)
